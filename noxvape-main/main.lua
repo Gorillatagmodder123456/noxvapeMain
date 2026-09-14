1497755857e670fbe0ea7d4aa356bb13fb62b463
Gorillatagmodder123456 <99627324+Gorillatagmodder123456@users.noreply.github.com>
2026-09-14 07:38:29 -0700

    Update BASE_URL and source retrieval in main.lua

--- noxvape-main/main.lua
+++ noxvape-main/main.lua
@@ -1,5 +1,7 @@
-local BASE_URL = "https://raw.githubusercontent.com/Gorillatagmodder123456/noxvapeMain/main/noxvape-main/main.lua"
-local source = game:HttpGet(BASE_URL .. "src/init.lua")
+local BASE_URL = "https://raw.githubusercontent.com/Gorillatagmodder123456/noxvapeMain/main/noxvape-main/"
+local source = game:HttpGet(BASE_URL .. "main.lua")
 local fn, err = loadstring(source)
+
 assert(fn, "noxvape bootstrap error: " .. tostring(err))
+
 return fn(BASE_URL)
