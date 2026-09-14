186dc001fba2635748b229c166a725c62db11dba
Gorillatagmodder123456 <99627324+Gorillatagmodder123456@users.noreply.github.com>
2026-09-14 07:40:15 -0700

    Change source file from main.lua to init.lua

--- noxvape-main/main.lua
+++ noxvape-main/main.lua
@@ -1,7 +1,8 @@
 local BASE_URL = "https://raw.githubusercontent.com/Gorillatagmodder123456/noxvapeMain/main/noxvape-main/"
-local source = game:HttpGet(BASE_URL .. "main.lua")
+
+local source = game:HttpGet(BASE_URL .. "src/init.lua")
 local fn, err = loadstring(source)
 
-assert(fn, "noxvape bootstrap error: " .. tostring(err))
+assert(fn, "noxvape init error: " .. tostring(err))
 
 return fn(BASE_URL)
