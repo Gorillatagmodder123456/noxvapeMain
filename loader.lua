19f011f438742c3bfd28057908bcda55a6e80ac3
Gorillatagmodder123456 <99627324+Gorillatagmodder123456@users.noreply.github.com>
2026-09-17 18:53:40 -0700

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
--- noxvape GitHub loader
--- Put this file anywhere you want to execute the project from.
-
-local BASE_URL = "https://raw.githubusercontent.com/Gorillatagmodder123456/noxvapeSRC/main/"
-local source = game:HttpGet(BASE_URL .. "src/init.lua")
-local chunk, err = loadstring(source)
-assert(chunk, "noxvape bootstrap error: " .. tostring(err))
-return chunk(BASE_URL)
